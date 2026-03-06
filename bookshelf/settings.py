ece0f1d078a34991f8bc985a23583dd102904782
user-deepak-09 <deepak2007cbe@gmail.com>
2026-03-06 18:38:42 +0530

    Add PostgreSQL support for production

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os  #read the environment variables
 from dotenv import load_dotenv 
+import dj_database_url
 
 # Load environment variables from .env file
 load_dotenv()
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'bookshelf.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db,sqlite3',
+        conn_max_age=600
+       )
 }
 
 
